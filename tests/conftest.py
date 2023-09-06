db91e6d928d074b656b028399df80686cbd83bee
enzymezoo-code <enzymezoo.art@gmail.com>
2023-09-06 13:32:57 -0500

    Ensure tests are run in correct order

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,7 @@ import pytest
 
 # Command line arguments for pytest
 def pytest_addoption(parser):
-    parser.addoption('--output_dir', action="store", default='tests/inference/samples', help='Output directory for generated images')
+    parser.addoption('--output_dir', action="store", default='tests/inference/samples/test', help='Output directory for generated images')
     parser.addoption("--listen", type=str, default="127.0.0.1", metavar="IP", nargs="?", const="0.0.0.0", help="Specify the IP address to listen on (default: 127.0.0.1). If --listen is provided without an argument, it defaults to 0.0.0.0. (listens on all)")
     parser.addoption("--port", type=int, default=8188, help="Set the listen port.")
 
@@ -18,3 +18,16 @@ def args_pytest(pytestconfig):
     os.makedirs(args['output_dir'], exist_ok=True)
 
     return args
+
+def pytest_collection_modifyitems(items):
+    # Modifies items so tests run in the correct order
+    
+    LAST_TESTS = ['test_quality']
+
+    module_name_map = {item.module.__name__: item for item in items}
+
+    # Move the last tests to the end
+    for test in LAST_TESTS:
+        if test in module_name_map:
+            items.append(module_name_map[test])
+            items.remove(module_name_map[test])
